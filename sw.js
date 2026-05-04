88cb1a25602c132e0db131ee9fff72b11c500fce
Marko Todorovic <todorovic.markonato88@gmail.com>
2026-05-04 11:30:20 +0200

    sw update

---
+++ sw.js
@@ -0,0 +1,30 @@
+const CACHE_NAME = 'chik-pogodi-v2';
+
+self.addEventListener('install', evt => {
+    self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(keys =>
+            Promise.all(
+                keys
+                    .filter(key => key !== CACHE_NAME)
+                    .map(key => caches.delete(key))
+            )
+        ).then(() => self.clients.claim())
+    );
+});
+
+// Network-first: always try network, fall back to cache only if offline
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        fetch(event.request)
+            .then(response => {
+                const clone = response.clone();
+                caches.open(CACHE_NAME).then(cache => cache.put(event.request, clone));
+                return response;
+            })
+            .catch(() => caches.match(event.request))
+    );
+});
